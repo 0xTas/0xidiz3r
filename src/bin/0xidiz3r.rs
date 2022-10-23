8fa96a363d31ae6d6412b7d7e10f34b4a4f7a910
0xTas <admin@0xtas.dev>
2022-10-22 21:55:36 -0700

    Massive refactor

--- src/bin/0xidiz3r.rs
+++ src/bin/0xidiz3r.rs
@@ -1,59 +1,24 @@
-use oxidizer::{input, define_batch_variable, CharSet};
+use oxidizer::input;
 use oxidizer::batch::BatchObfuscator;
-use std::fs::{self, File};
-use std::io::Write;
+use std::fs;
 
-
-// TODO: Handle batch command length limit of 8191 bytes with dynamic payload-length adjustments?
-fn create_obfuscated_batch(src: &str) {
-    let obfuscator: BatchObfuscator = BatchObfuscator::new(None, None);
-
-    let mut commands: Vec<String> = Vec::new();
-    let mut execute: Vec<String> = Vec::new(); 
-
-    commands.push(String::from(":: VGhpcyBmaWxlIHdhcyBvYmZ1c2NhdGVkIHZpYSBodHRwczovL2dpdGh1Yi5jb20vMHhUYXMvMHhpZGl6M3I="));
-    commands.push(String::from(":: VGhpcyBmaWxlIGNhbiBiZSBwcm9ncmFtYXRpY2FsbHkgZGVvYmZ1c2NhdGVkIChzb29u4oSiKSB2aWEgaHR0cHM6Ly9naXRodWIuY29tLzB4VGFzLzB4aWRpejNy"));
-    commands.push(String::from("@echo off"));
-    commands.push(format!("set {}=set", obfuscator.set_str));
-    commands.push(format!("%{}% {}= ", obfuscator.set_str, obfuscator.space_str));
-    commands.push(format!("%{}%%{}%{}==", obfuscator.set_str, obfuscator.space_str, obfuscator.eq_str));
-
-    for chr in src.chars() {
-
-        if !CharSet::FullSet.values().contains(&chr) {
-            execute.push(format!("{}", chr.to_owned()));
-        }else if !CharSet::BadChars.values().contains(&chr) {
-            let varname: &String = obfuscator.alphabet.get(&chr).expect("Key not in alphabet!");
-
-            commands.push(define_batch_variable(format!("{}", varname.to_owned()), format!("{}", chr.to_owned()), &obfuscator));
-            execute.push(format!("%{}%", varname.to_owned()));
-        }else {
-            execute.push(format!("{}", chr.to_owned()));
-        }
-
-    };
-
-    let execute_string: String = execute.join("");
-    commands.push(execute_string);
-    commands.push(String::from(":: This file was obfuscated via https://github.com/0xTas/0xidiz3r "));
-    commands.push(String::from(":: This file can be programatically deobfuscated (soon™) via https://github.com/0xTas/0xidiz3r "));
-
-    let final_code: String = commands.join("\n");
-    let mut file = File::create("obfuscated.bat").expect("Failed to create file!");
-    file.write_all(final_code.as_bytes()).expect("Failed writing to file!");
-}
 
 fn main() {
     // let poc: &str = "start C:/WINDOWS/System32/calc.exe";
+
+    let mut obfuscator: BatchObfuscator = BatchObfuscator::new();
 
     let poc: String = input("Enter Batch Command or Path to File ~> ");
 
     let file_check = poc.clone();
     if let Ok(contents) = fs::read_to_string(file_check.as_str().trim_end()) {
-        create_obfuscated_batch(contents.as_str().trim_end());
+        obfuscator.initialize(None, None, contents.as_str().trim_end());
+        let path = obfuscator.write_obfuscated_script(None);
+        println!("Obfuscation Complete.\n{}", path);
+        
     }else {
-        create_obfuscated_batch(poc.as_str().trim_end());
+        obfuscator.initialize(None, None, poc.as_str().trim_end());
+        let path = obfuscator.write_obfuscated_script(None);
+        println!("Obfuscation Complete.\n{}", path);
     };
-
-    println!("Obfuscation Complete.");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,65 +1,38 @@
 use std::io::{self, Write};
-
-use rand::seq::SliceRandom;
-use rand::{thread_rng, Rng};
-use batch::BatchObfuscator;
+use std::collections::HashSet;
+use rand::{thread_rng, Rng, prelude::SliceRandom};
+use batch::CharSet;
 
 pub mod batch;
 
-#[derive(Debug)]
-pub enum CharSet {
-    FullSet,
-    Letters,
-    BadChars
-}
 
-impl CharSet {
-    pub fn values(&self) -> Vec<char> {
-        match *self {
-            CharSet::FullSet => vec!['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
-                            'v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
-                            'R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','"','#',
-                            '$','%','&','\'','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\\',
-                            ']','^','_','`','{','|','}','~',' ', '\n', '\r'],
-
-            CharSet::Letters => vec!['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
-                            'v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
-                            'R','S','T','U','V','W','X','Y','Z'],
-
-            CharSet::BadChars => vec!['<','>','|','%','^','&', '\n', '\r']
-        }
-    }
-}
-
+/* General Utility Functions */
 
 /// Returns a string of a random length between min/max containing random ascii letters (mixed case).<br>
 /// Min default value is 69.<br>
 /// Max default value is 209 (*exceeding this value is not recommended*).<br>
 /// Call with None (e.g **generate_random_chars(None, None)** to use default values).<br>
 /// **Using default values is recommended unless you intend to make them smaller.**
-pub fn generate_random_chars(min: Option<u32>, max: Option<u32>) -> String {
+pub fn generate_random_chars(min: Option<u32>, max: Option<u32>, used: &HashSet<String>) -> String {
     // Functionally-default values for min and max lengths.
     let min_len: u32 = min.unwrap_or(7);
     let max_len: u32 = max.unwrap_or(109);
 
     let mut rng_chars: Vec<char> = Vec::new();
     let mut rng = thread_rng();
+
     for _ in 0..=thread_rng().gen_range(min_len..=max_len) {
         rng_chars.push(*CharSet::Letters.values().choose(&mut rng).expect("CharSet should not be empty!"));
     };
 
     let rng_string: String = rng_chars.into_iter().collect();
 
-    rng_string
+    if !used.contains(&rng_string) {
+        return rng_string;
+    }else {
+        return generate_random_chars(Some(min_len), Some(max_len), used);
+    };
 }
-
-/// Returns an obfuscated string representing a variable definition statement in Batch.
-pub fn define_batch_variable(name: String, value: String, prelude: &BatchObfuscator) -> String {
-    format!("%{}%%{}%{}%{}%{}", prelude.set_str, prelude.space_str, name, prelude.eq_str, value)
-}
-
-
-/* General Utility Functions */
 
 pub fn input(prompt: &str) -> String {
     let mut user_input: String = String::new();
@@ -71,45 +44,4 @@ pub fn input(prompt: &str) -> String {
         .expect("Read from stdin failed!");
 
     user_input
-}
-
-
-#[cfg(test)]
-mod tests {
-    use std::io::Write;
-
-    use super::*;
-
-    #[test]
-    fn test_charset() {
-        println!("Full_Charset: {:#?}\n Letters: {:#?}\n, BadChars: {:#?}", CharSet::FullSet.values(), CharSet::Letters.values(), CharSet::BadChars.values());
-    }
-
-    #[test]
-    fn test_prelude() {
-        let obfuscator: BatchObfuscator = BatchObfuscator::new(None, None);
-
-        println!("{:#?}", obfuscator);
-    }
-
-    #[test]
-    fn test_collisions() {
-        for _ in 0..10 {
-            let mut used_strings: Vec<String> = Vec::new();
-            let mut collisions: Vec<String> = Vec::new();
-            #[allow(non_snake_case)]
-            for N in 0..=10000 {
-                let random_chars: String = generate_random_chars(None, None);
-
-                if used_strings.contains(&random_chars) {
-                    collisions.push(random_chars);
-                }else {
-                    used_strings.push(random_chars);
-                }
-                print!("Iteration: {}\r", N);
-                std::io::stdout().flush().expect("Write to console failed!");
-            };
-            println!("Number of Collisions: {}", collisions.len());
-        };
-    }
 }
