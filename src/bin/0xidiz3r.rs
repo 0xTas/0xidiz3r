d071984ab42ceb57d590bc07d09fbd48515e8447
0xTas <admin@0xtas.dev>
2022-10-22 22:05:02 -0700

    Refactored variable name

--- src/bin/0xidiz3r.rs
+++ src/bin/0xidiz3r.rs
@@ -8,16 +8,16 @@ fn main() {
 
     let mut obfuscator: BatchObfuscator = BatchObfuscator::new();
 
-    let poc: String = input("Enter Batch Command or Path to File ~> ");
+    let target: String = input("Enter Batch Command or Path to File ~> ");
 
-    let file_check = poc.clone();
+    let file_check = target.clone();
     if let Ok(contents) = fs::read_to_string(file_check.as_str().trim_end()) {
         obfuscator.initialize(None, None, contents.as_str().trim_end());
         let path = obfuscator.write_obfuscated_script(None);
         println!("Obfuscation Complete.\n{}", path);
         
     }else {
-        obfuscator.initialize(None, None, poc.as_str().trim_end());
+        obfuscator.initialize(None, None, target.as_str().trim_end());
         let path = obfuscator.write_obfuscated_script(None);
         println!("Obfuscation Complete.\n{}", path);
     };
