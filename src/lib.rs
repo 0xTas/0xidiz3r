b287aeca4d5686b70320c9e88f9ddb991d42ce3e
0xTas <admin@0xtas.dev>
2022-10-22 22:45:23 -0700

    Docstrings

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ pub mod batch;
 
 /* General Utility Functions */
 
+/// Prompts the user for input and returns it as a String.<br><br>
+/// The *prompt: &str* parameter is printed without a trailing newline by default. Supply your own if desired.
 pub fn input(prompt: &str) -> String {
     let mut user_input: String = String::new();
 
