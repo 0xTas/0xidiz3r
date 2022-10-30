6ce4c6171e955d0bc411b07dc1f68e4b1a8a84e2
0xTas <admin@0xtas.dev>
2022-10-30 07:52:02 -0700

    Added clap for argument parsing

--- src/bin/0xidiz3r.rs
+++ src/bin/0xidiz3r.rs
@@ -1,57 +1,85 @@
-use oxidizer::input;
+use oxidizer::wait;
 use oxidizer::batch::obfuscator::BatchObfuscator;
 use oxidizer::batch::deobfuscator::BatchDeobfuscator;
-use std::fs;
+use clap::Parser;
+use std::{fs, process::exit};
 
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+
+    /// Input File or Command
+    input: String,
+
+    /// Use deobfuscation mode
+    #[arg(short, long, default_value_t = false)]
+    deobfuscate: bool,
+
+    /// Don't add "@echo off" to the output script.
+    #[arg(long, default_value_t = true)]
+    echo_off: bool,
+
+    /// Custom name for the output file
+    #[arg(short, long)]
+    output_file: Option<String>,
+
+    /// Minimum obfuscated variable length
+    #[arg(long)]
+    min: Option<u32>,
+
+    /// Maximum obfuscated variable length
+    #[arg(long)]
+    max: Option<u32>
+}
 
 fn main() {
-    // let poc: &str = "start C:/WINDOWS/System32/calc.exe";
+    
+    let mut args = Args::parse();
 
-    let mode: String = input("Default mode is obfuscate. Switch to deobfuscate mode? [Y/N] ~> ");
-
-    if mode.to_lowercase().contains("y") {
-            let mut deobfuscator = BatchDeobfuscator::new();
-
-            let target: String = input("Enter Path to Obfuscated File ~> ");
-
-            let file_check = target.clone();
-            if let Ok(contents) = fs::read_to_string(file_check.as_str().trim_end()) {
-                deobfuscator.initialize(contents);
-
-                let path: String = deobfuscator.write_deobfuscated_script(None);
-                println!("\nDumped deobfuscated output to file: {}\nDeobfuscation Complete.", path);
-            }else {
-                panic!("File not found!");
+    if let Some(min_value) = args.min {
+        if let Some(max_value) = args.max {
+            if min_value >= max_value {
+                args.max = Some(args.min.unwrap() * 2);
             };
-    }else {
-
-        let mut obfuscator: BatchObfuscator = BatchObfuscator::new();
-
-        let target: String = input("Enter Batch Command or Path to File ~> ");
-
-        let file_check = target.clone();
-        if let Ok(contents) = fs::read_to_string(file_check.as_str().trim_end()) {
-            if contents.to_lowercase().contains("echo") && !contents.to_lowercase().contains("@echo off") {
-                println!("\n[!]--> INFO: By default, \"@echo off\" is inserted to aid the obfuscation.");
-                let cont: String = input("Would you like to preserve the script's cmd echo functionality instead? [Y/N] ~> ");
-
-                if cont.to_lowercase().contains("y") {
-                    obfuscator.enable_echo();
-                };
-            };
-
-            obfuscator.initialize(None, None, contents.trim_end().to_string());
-            let path = obfuscator.write_obfuscated_script(None);
-            println!("\nDumped obfuscated output to file: {}\nObfuscation Complete.", path);
-            
-        }else {
-            obfuscator.initialize(None, None, target.trim_end().to_string());
-            let path = obfuscator.write_obfuscated_script(None);
-            println!("\nDumped obfuscated output to file: {}\nObfuscation Complete.", path);
         };
     };
 
+    if args.deobfuscate {
+        let mut deobfuscator: BatchDeobfuscator = BatchDeobfuscator::new();
 
-    
+        if let Ok(contents) = fs::read_to_string(args.input.trim_end()) {
+            deobfuscator.initialize(contents);
+        }else {
+            println!("\nError! File not found.");
+            println!("Please provide a valid path to an obfuscated file!");
+            wait(4200);
+            return main();
+        };
 
+        let path: String = deobfuscator.write_deobfuscated_script(args.output_file);
+        println!("\nDumped deobfuscated output to file: {}\nDeobfuscation Complete.", path);
+
+
+        exit(0);
+    }else {
+        let mut obfuscator :BatchObfuscator = BatchObfuscator::new();
+        if !args.echo_off {
+            obfuscator.enable_echo();
+        };
+
+        if let Ok(contents) = fs::read_to_string(args.input.trim_end()) {
+            obfuscator.initialize(args.min, args.max, contents);
+
+            let path: String = obfuscator.write_obfuscated_script(args.output_file);
+            println!("\nDumped obfuscated output to file: {}\nObfuscation Complete.", path);
+        }else {
+            obfuscator.initialize(args.min, args.max, args.input);
+
+            let path: String = obfuscator.write_obfuscated_script(args.output_file);
+            println!("\nDumped obfuscated output to file: {}\nObfuscation Complete.", path);
+        };
+
+        exit(0);
+    };
 }
